ed61a02f870b010a9ee2cc8b78b3399aa525041c
Bumpfel <34800245+Bumpfel@users.noreply.github.com>
2019-10-11 17:30:29 +0200

    a2-hashtable working. messy

--- A2/src/assignment2AADS/assignment2/FindPrime.java
+++ A2/src/assignment2AADS/assignment2/FindPrime.java
@@ -5,16 +5,15 @@ public class FindPrime {
   public static void main(String[] args) {
 
     long timestamp = System.currentTimeMillis();
-    int prime = 1; //, i = 0;
+    // int prime = 1; //, i = 0;
     // while(i < 1000) {
-    for(int i = 0; i < 1000; i++) {
-      prime = findNextPrime(prime);
-      System.out.println(prime);
-      i ++;
-    }
-    // printPrimeSieve(30);
+    // for(int i = 0; i < 10; i++) {
+    //   prime = findNextPrime(prime + 1);
+    //   System.out.println(prime);
+    // }
+    printPrimeSieve(20);
     long timeTaken = System.currentTimeMillis() - timestamp;
-    System.out.println("----time taken: " + timeTaken + "ms");
+    // System.out.println("----time taken: " + timeTaken + "ms");
 
   }
 
@@ -30,21 +29,19 @@ public class FindPrime {
           prime[i] = false;
         }
       }
+    }
 
-      for (int i = 2; i <= n; i++) {
-        if (prime[i])
-          System.out.println(i);
-      }
+    for (int i = 2; i <= n; i++) {
+      if (prime[i])
+        System.out.println(i);
     }
   }
 
-
   private static int findNextPrime(int n) {
-    n ++;
     if (isPrime(n)) {
       return n;
     }
-    return findNextPrime(n);
+    return findNextPrime(n + 1);
   }
  
   private static boolean isPrime(int n) {
--- A2/src/assignment2AADS/assignment2/MyHashTable.java
+++ A2/src/assignment2AADS/assignment2/MyHashTable.java
@@ -1,16 +1,20 @@
 package assignment2AADS.assignment2;
 
+import java.util.ArrayList;
+
 public class MyHashTable<T> implements A2HashTable<T> {
   private final double MAX_LOAD;
   // private Object[] elements;
 
   private T[] elements;
+  // private Node[] elements;
   private int size;
   
   @SuppressWarnings("unchecked")
-  public MyHashTable(double _maxLoad) {
-    MAX_LOAD = _maxLoad;
+  public MyHashTable(double maxLoad) {
+    MAX_LOAD = maxLoad;
     elements = (T[]) new Object[11];
+    // elements = (Node[]) new Object[11];
   }
 
   // public MyHashTable(int initialCapacity, double _maxLoad) {
@@ -23,22 +27,55 @@ public class MyHashTable<T> implements A2HashTable<T> {
   // }
 
   public static void main(String[] args) {
-    MyHashTable<String> sut = new MyHashTable<>(.75);
+    MyHashTable<Object> sut = new MyHashTable<>(.75);
+
+    // sut.insert(1);
+
+    // sut.insert(obj1);
+    // System.out.println("Contains " + obj2 + ": " + sut.contains(obj2));
+
+
+    String obj1 = new String("din");
+    String obj2 = new String("din");
+
+    for(int i = 65; i < 200; i++) {
+      sut.insert((char) i);
+    }
+
     sut.insert("hej");
     sut.insert("på");
     sut.insert("dig");
-    sut.insert("din");
     sut.insert("lille");
     sut.insert("sociopat");
     sut.insert(",");
     sut.insert("ge");
     sut.insert("mig");
+    // sut.insert("din");
+    // sut.insert("din");
+    sut.insert("din");
+    sut.insert(obj1);
+    sut.insert(obj2);
     sut.insert("min");
     sut.insert("uppgift");
     sut.insert("!");
-    System.out.println();
-    System.out.println("EVERYTHING INSERTED. PRINTING TABLE");
-    System.out.println(sut.toString());
+
+    // sut.insert("bla");
+    // sut.insert("bla");
+    // sut.insert("bla");
+    // sut.insert("bla");
+    // sut.insert("bla");
+    // sut.insert("bla");
+    // sut.insert("bla");
+    // sut.insert("bla");
+    // sut.insert("bla");
+    // sut.insert("bla");
+
+    // System.out.println();
+    // System.out.println("EVERYTHING INSERTED. PRINTING TABLE");
+    // System.out.println(sut.toString());
+
+    Object searchObj = obj2;
+    System.out.println("Contains " + searchObj + ": " + sut.contains(searchObj));
 
     // int prime = 11;
     // for(int i = 0; i < 7; i++) {
@@ -50,18 +87,11 @@ public class MyHashTable<T> implements A2HashTable<T> {
   @Override
   public void insert(T element) {
     double currentLoad = (double) size / elements.length;
-    if(currentLoad > MAX_LOAD) {
-      System.out.println("-### Exceeded max load, re-hashing");
+    // Node n = new Node(element);
+    int key = findFreeCell(element);
+    if (currentLoad > MAX_LOAD || key < 0) {
+      System.out.println("-### Exceeded max load, OR did not find empty cell. Re-hashing");
       System.out.println(size + ", " + elements.length + " == " + currentLoad);
-      rehash();
-      insert(element);
-      return;
-    }
-
-    Node n = new Node(element);
-    int key = n.hashCode();
-    if(key < 0) {
-      System.out.println("Did not find empty cell, re-hashing");
       rehash();
       insert(element);
       return;
@@ -73,13 +103,31 @@ public class MyHashTable<T> implements A2HashTable<T> {
 
   @Override
   public void delete(T element) {
-    // TODO Auto-generated method stub
+    int index = indexOf(element);
+    elements[index] = null;
   }
 
   @Override
   public boolean contains(T element) {
-    // TODO Auto-generated method stub
-    return false;
+    return indexOf(element) >= 0;
+  }
+
+  public int indexOf (T element) {
+    int hash = element.hashCode();
+    int i = 0;
+    int key;
+    while (i < elements.length) {
+      key = getKey(hash, i);
+      if (key < 0 || elements[key] == null) {
+        break;
+      }
+      if (elements[key] == element) {
+        System.out.println("found after " + (i + 1) + " iterations");
+        return key;
+      }
+      i ++;
+    }
+    return -1;
   }
 
   @Override
@@ -90,29 +138,56 @@ public class MyHashTable<T> implements A2HashTable<T> {
   @SuppressWarnings("unchecked")
   private void rehash () {
     T[] oldArray = elements;
-    int nextPrime = findNextPrime(elements.length * 2);
+    int nextPrime = findNextPrimeFrom(elements.length * 2 + 1);
     elements = (T[]) new Object[nextPrime];
 
     size = 0;
-    for(T o : oldArray) {
-      if(o != null) {
-        insert(o);
+    for (T obj : oldArray) {
+      if (obj != null) {
+        insert(obj);
       }
     }
     System.out.println("REHASH DONE, NEW SIZE " + elements.length + "\n" + this);
   }
 
-  private int findNextPrime(int n) {
-    n ++;
+  private int findFreeCell(T obj) {
+    int hash = Math.abs(obj.hashCode());
+    // int hash = hashCode & 0x7FFFFFFF;
+
+    int i = 0;
+    int key;
+    // System.out.println("----- " + obj + " " + hash + " --------");
+    do {
+      key = getKey(hash, i);
+      // key = (hash + (int) Math.pow(i, 2) + i) / 2 % elements.length;
+
+      // System.out.println("checking key " + key);
+      // if (Math.pow(i, 2) >= elements.length) {
+      if (i == elements.length) { // no empty cell could be found
+        // System.out.println("###### COULD NOT FIND EMPTY CELL");
+        return -1;
+      }
+      i++;
+    }
+    while(elements[key] != null);
+
+    return key;
+  }
+
+  private int getKey (int hash, int i) {
+    return (hash + (int) Math.pow(i, 2)) % elements.length;
+  }
+
+  private int findNextPrimeFrom(int n) {
     if (isPrime(n)) {
       return n;
     }
-    return findNextPrime(n);
+    return findNextPrimeFrom(n + 1);
   }
  
   private boolean isPrime(int n) {
-    for(int i = 2; i < n; i++) {
-      if(n % i == 0) {
+    for (int i = 2; i < n; i++) {
+      if (n % i == 0) {
         return false;
       }
     }
@@ -122,7 +197,7 @@ public class MyHashTable<T> implements A2HashTable<T> {
   @Override
   public String toString () {
     StringBuilder str = new StringBuilder();
-    for(int i = 0; i < elements.length; i ++) {
+    for (int i = 0; i < elements.length; i ++) {
       str.append("[" + i + "] ");
       if (elements[i] != null) {
          str.append(elements[i]);
@@ -132,41 +207,28 @@ public class MyHashTable<T> implements A2HashTable<T> {
     return str.toString();
   }
 
-  class Node {
-    T value;
+  // class Node {
+  //   T element;
+  //   int key;
 
-    Node (T _value) {
-      value = _value;
-    }
+  //   Node (T _element, int _key) {
+  //     element = _element;
+  //     key = _key;
+  //   }
 
-    @Override
-    public boolean equals (Object o) {
-      return value.equals(o.toString());
-    }
+  //   @SuppressWarnings("unchecked")
+  //   @Override
+  //   public boolean equals (Object o) {
+  //     Node obj = (Node) o;
+  //     return element == obj && key == obj.key;
+  //     // return element.equals((T) o);
+  //   }
 
-    @Override
-    public int hashCode () {
-      int hash = Math.abs(value.hashCode());
-
-      int key;
-      int i = 0;
-      System.out.println("----- " + value + " " + hash + " --------");
-      do {
-        key = (hash + (int) Math.pow(i, 2)) % elements.length;
-        // key = (hash + (int) Math.pow(i, 2) + i) / 2 % elements.length;
-
-        System.out.println("checking key " + key);
-        // if (Math.pow(i, 2) >= elements.length) {
-        if (i == elements.length) { // no empty cell could be found
-          System.out.println("###### COULD NOT FIND EMPTY CELL");
-          return -1;
-        }
-        i++;
-      }
-      while(elements[key] != null);
-  
-      return key;
-    }
-  }
+  //   @Override
+  //   public int hashCode () {
+  //     return element.hashCode();
+  //   }
+    
+  // }
 
 }
